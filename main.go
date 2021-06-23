4e71b16b3badd00456bd1c14bf47d1c2c173d514
Hugo Zhu <hugozhu@gmail.com>
2021-06-23 20:50:00 +0800

    removed corpid and corpsecret requirement

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 
 	dingtalk "github.com/hugozhu/godingtalk"
@@ -19,24 +18,12 @@ func init() {
 
 func main() {
 	title := githubactions.GetInput("title")
-	corpid := githubactions.GetInput("corpid")
-	corpsecret := githubactions.GetInput("corpsecret")
+	msgtype := githubactions.GetInput("type")
 	token := githubactions.GetInput("token")
-
 	file := githubactions.GetInput("file")
 
 	if title == "" {
 		githubactions.Fatalf("missing 'title'")
-		return
-	}
-
-	if corpid == "" {
-		githubactions.Fatalf("missing 'corpid'")
-		return
-	}
-
-	if corpsecret == "" {
-		githubactions.Fatalf("missing 'corpsecret'")
 		return
 	}
 
@@ -57,10 +44,16 @@ func main() {
 	}
 
 	c := dingtalk.NewDingTalkClient("", "")
-	resp, err := c.SendRobotMarkdownMessage(token, title, "# "+title+"\n\n"+message)
+
+	var err error
+
+	if msgtype != "text" {
+		_, err = c.SendRobotTextMessage(token, title+"\n\n"+message)
+	} else {
+		_, err = c.SendRobotMarkdownMessage(token, title, "# "+title+"\n\n"+message)
+	}
+
 	if err != nil {
 		githubactions.Fatalf("failed to send dingtalk message, %v", err)
-	} else {
-		githubactions.AddMask(fmt.Sprintf("%v", resp.OAPIResponse))
 	}
 }
